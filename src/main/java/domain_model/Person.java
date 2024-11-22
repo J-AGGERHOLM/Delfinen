7a1899043ae3fe161f8bdac7ece82cba1c726027
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2024-11-22 10:25:02 +0100

    started work on teams

--- src/main/java/domain_model/Person.java
+++ src/main/java/domain_model/Person.java
@@ -1,4 +1,8 @@
 package domain_model;
 
 public class Person {
+    private String ID;
+    private String name;
+
+
 }
--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,4 +1,26 @@
 package ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    public void mainLoopBenjamin(){
+
+        boolean exit = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, and welcome to the Doplhin swimclub admin program! please enter a command");
+
+        while(!exit){
+            String userChoice = sc.nextLine();
+            switch (userChoice.toUpperCase()){
+                case "EXIT" -> exit = true;
+                case "DISPLAY TEAM" -> displayTeams();
+                default -> System.out.println("Please enter a valid Command");
+            }
+        }
+    }
+
+    private void displayTeams(){
+
+    }
 }
