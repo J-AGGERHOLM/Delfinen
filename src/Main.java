468d69cdd75664752ac2eedcaf3f54d6932e0e80
mathiasege <macl0001@stud.kea.dk>
2024-11-22 09:32:43 +0100

    Installeret JDK

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main{
    public static void main(String[] args) {
        System.out.println("Jack");
        System.out.println("Welcome to the dolphin project");
-        System.out.println("My push :D");
+       System.out.println("My push :D");
     }
 }
