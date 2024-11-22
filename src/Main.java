da75e783fea6e06161c2dc6c9da09b25dfca4d87
HastingsJack <joha0005@stud.kea.dk>
2024-11-22 09:16:41 +0100

    Jack Test Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("Welcome to the dolphin project");
+   public static void main(String[] args) {
+       System.out.println("Jack");
+       System.out.println("Welcome to the dolphin project");
         System.out.println("My push :D");
     }
-
 }
