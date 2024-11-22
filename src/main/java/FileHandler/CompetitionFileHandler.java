1033b50c918b9445966ad0160fc899521bae67fa
J-AGGERHOLM <jajo0004@stud.kea.dk>
2024-11-22 14:16:10 +0100

    file create competion added to UI

--- src/main/java/FileHandler/CompetitionFileHandler.java
+++ src/main/java/FileHandler/CompetitionFileHandler.java
@@ -13,13 +13,24 @@ public class CompetitionFileHandler extends SuperHandler {
 
     final String filePath = "CompetitionResults.txt";
 
+
+    public void setCompetition(Competition competition) {
+        this.competition = competition;
+    }
+
+
+
     @Override
     public void create() throws IOException {
+        if (competition == null) {
+            throw new IllegalStateException("Competition entry is empty.");
+        }
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
         writer.write(competition.toString());
         writer.newLine();
         writer.flush();
     }
+
 
     @Override
     public void read() {
--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,32 +1,77 @@
 package ui;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import FileHandler.CompetitionFileHandler;
+import FileHandler.SuperHandler;
+import Models.Competition;
+
 
 public class UserInterface {
 
-    public void mainLoopBenjamin(){
+    SuperHandler competitionFileHandler = new CompetitionFileHandler();
+
+
+
+
+    public void mainLoopBenjamin() {
 
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello, and welcome to the Doplhin swimclub admin program! please enter a command");
 
-        while(!exit){
+        while (!exit) {
             String userChoice = sc.nextLine();
-            switch (userChoice.toUpperCase()){
+            switch (userChoice.toUpperCase()) {
                 case "EXIT" -> exit = true;
                 case "DISPLAY TEAM" -> displayTeams();
+                case "COMPETITION" -> displayCompetion();
                 default -> System.out.println("Please enter a valid Command");
             }
         }
     }
 
-    private void displayTeams(){
+    private void displayTeams() {
         System.out.println("Please select the team you wish to view:");
 
         //TODO: here we display a list of teams
     }
 
-    private void displayMembers(){
+    private void displayMembers() {
         // show a list of all members
+    }
+
+    private void displayCompetion()  {
+        Scanner sc = new Scanner(System.in);
+        Scanner intScanner = new Scanner(System.in);
+        Scanner doubleScanner = new Scanner(System.in);
+
+        System.out.println("You are displaying the competitions");
+        System.out.println("You now have the following options:");
+        System.out.println("CREATE : To create a competition entry");
+
+        String competitionInput = sc.nextLine();
+
+
+        switch (competitionInput.toUpperCase()) {
+            case "CREATE" -> {
+                System.out.println("Please enter the name of the event:");
+                String event = sc.nextLine();
+                System.out.println("Please enter te placement achieved:");
+                int placement = intScanner.nextInt();
+                System.out.println("Please enter the swimmers time:");
+                double time = doubleScanner.nextDouble();
+                Competition competition = new Competition(event,placement,time);
+                ((CompetitionFileHandler) competitionFileHandler).setCompetition(competition);
+
+                try {
+                    competitionFileHandler.create();
+                } catch (IOException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+            default -> System.out.println("Not an option");
+        }
     }
 }
