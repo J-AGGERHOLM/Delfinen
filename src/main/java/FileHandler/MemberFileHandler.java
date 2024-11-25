08a3927dfbaff42b4759a10d9bbfb471c75660e8
mathiasege <macl0001@stud.kea.dk>
2024-11-25 10:27:48 +0100

    Prøvet at merge 2.0

--- src/main/java/FileHandler/MemberFileHandler.java
+++ src/main/java/FileHandler/MemberFileHandler.java
@@ -1,6 +1,27 @@
 package FileHandler;
 
+import java.io.IOException;
+
 public class MemberFileHandler extends SuperHandler{
 
 
+    @Override
+    public void create() throws IOException {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
