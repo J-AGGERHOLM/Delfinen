133c00b9ca48fb26d6e60b1f4b4875746c128891
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2024-11-27 11:39:30 +0100

    added unversioned files that showed up

---
+++ src/main/java/Repositories/CompetitionRepository.java
@@ -0,0 +1,34 @@
+package Repositories;
+
+import FileHandler.CompetitionFileHandler;
+import Models.Competition;
+
+import java.io.IOException;
+
+public class CompetitionRepository {
+
+    CompetitionFileHandler competitionFileHandler = new CompetitionFileHandler();
+
+
+
+
+    public void commitCompetitionEntry(String event, int placement, double time) {
+        Competition competition = new Competition(event, placement, time);
+
+        competitionFileHandler.setCompetition(competition);
+
+        //Competition object is comitted to the document
+        try {
+            competitionFileHandler.create();
+        } catch (IOException e) {
+            System.out.println("Error: Something went wrong trying to create the file");
+        }
+
+    }
+
+
+    public void searchForEntry(String eventName){
+        competitionFileHandler.delete();
+    }
+
+}
