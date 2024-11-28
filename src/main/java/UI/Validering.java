6fed25d38f98bd796cce2e458826800e56c2c7e3
mathiasege <macl0001@stud.kea.dk>
2024-11-28 17:24:36 +0100

    Tilføjet test og validering

--- src/main/java/UI/Validering.java
+++ src/main/java/UI/Validering.java
@@ -1,76 +1,56 @@
 package UI;
 
-import java.util.InputMismatchException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Validering {
-    /*
-
-
-    We might end up with creating a scanner for each method.
-    Sometimes the scanner acts funny. If your input doesn't get validated
-    you need to create it locally in the method instead of globally
-
-
-     */
-    Scanner scan;
-    public Validering(){
-        scan = new Scanner(System.in);
-    }
 
     //method to check if a string variable is empty.
-    //currently only used to make sure that there are no movie entries without titles.
-    public String checkString(String value) {
-        while (value.isEmpty()) {
+    //currently only used to make sure that there are no entries without data.
+    public static String checkString(Scanner scan) {
+        // Trims the data.
+        String input = scan.nextLine().trim();
+        while (input.isEmpty()) {
             System.out.println("Please enter some text");
-            value = scan.nextLine();
+            input = scan.next();
         }
-        return value;
-    }
 
-    // controls that inputted value is a string.
-    // if it is an integer it promts you to change it to a string.
-    // used to make sure we don't get any unwanted inputs while creating a movie entry.
-    public String isAString(String value) {
-        if (isAnInteger(value)) {
-            System.out.println("Please enter a valid Name");
-            value = scan.nextLine();
-            isAString(value);
-        }
-        return value;
+        return input;
     }
-
 
     //checks input to see if it is an integer.
     //helper method to use with isAString() method.
-    public boolean isAnInteger(String value) {
-        try {
-            Integer.parseInt(value);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+    public static String mustBeString(Scanner scan) {
+        while(scan.hasNextInt()){
+            System.out.println("You cant type a number");
+            scan.next();
         }
+        return scan.nextLine();
     }
 
-    // check for isInColor
-    public String stringIsYesNo(String value) {
-        // if user input != YES or NO, then the method prompts you to write a valid input.
-        while (!value.equals("YES") && !value.equals("NO")) {
-            System.out.println("Try again. Type: yes or no.");
-            value = scan.nextLine().toUpperCase().trim();
+    // Check for int value.
+    public static int checkInt(Scanner scan) {
+        while(!scan.hasNextInt()){
+            System.out.println("Please enter a number");
+            scan.next();
         }
-
-        return value;
+        return scan.nextInt();
     }
 
-    // Check for min value.
-    public int checkInt() {
-        try {
-            //makes sure it's an integer:
-            return Integer.parseInt(scan.nextLine());
-        } catch (InputMismatchException | NumberFormatException e) {
-            System.out.println("Please enter a valid number.");
-            return checkInt();
+    // Check for double value.
+    public static double checkDouble(Scanner scan) {
+        while(!scan.hasNextDouble()){
+            System.out.println("Please enter a number");
+            scan.next();
+        }
+        return scan.nextDouble();
+    }
+
+    public static LocalDate checkDate(int year, int month, int day) {
+        if (year > 1900 && month <= 12 && day <= 31){
+            return null;
+        }else{
+            return null;
         }
     }
 
---
+++ src/main/test/java/UI/ValideringTest.java
@@ -0,0 +1,117 @@
+package UI;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+
+public class ValideringTest {
+
+    @Disabled
+    void checkString() {
+        // Arrange:
+        Scanner scan = new Scanner("Test");
+        // Act:
+        String actual = Validering.checkString(scan);
+        String expected = "Test";
+        // Assert:
+        assertEquals(expected, actual);
+    }
+
+    // You have to debug to se it works.
+    // First it will take the empty space, go into while
+    // Next it will take the value and return
+    @Disabled
+    void checkStringNot() {
+        // Arrange:
+        Scanner scan = new Scanner("\n\nHallo");
+        // Act:
+        String actual = Validering.checkString(scan);
+        String expected = "Hallo";
+        // Assert:
+        assertEquals(expected, actual);
+    }
+
+    @Disabled
+    void mustBeString() {
+        // Arrange:
+        Scanner scan = new Scanner("Hallo");
+        // Act:
+        String actual = Validering.mustBeString(scan);
+        String expected = "Hallo";
+        // Assert:
+        assertEquals(expected, actual);
+    }
+
+    // You have to debug to se it works.
+    // First it will take the number, go into while
+    // Next it will take the value and return
+    @Disabled
+    void mustBeStringNot() {
+        // Arrange:
+        Scanner scan = new Scanner(1 + " Hallo");
+        // Act:
+        String actual = Validering.mustBeString(scan);
+        String expected = "Hallo";
+        // Assert:
+        assertEquals(expected, actual.trim());
+    }
+
+    @Disabled
+    void checkInt() {
+        // Arrange
+        Scanner scan = new Scanner(String.valueOf(1));
+        // Act
+        int actual = Validering.checkInt(scan);
+        int expected = 1;
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    // You have to debug to se it works.
+    // First it will take the value hej, go into while
+    // Next it will take the value and return
+    @Disabled
+    void checkIntNot() {
+        // Arrange
+        Scanner scan = new Scanner("hej " + 1);
+        // Act
+        int actual = Validering.checkInt(scan);
+        int expected = 1;
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkDouble() {
+        // Arrange
+        Scanner scan = new Scanner(String.valueOf(2.2));
+        // Act
+        double actual = Validering.checkDouble(scan);
+        double expected = 2.2;
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    // You have to debug to se it works.
+    // First it will take the value hej, go into while
+    // Next it will take the value and return
+    @Test
+    void checkDoubleNot() {
+        // Arrange
+        Scanner scan = new Scanner("\n" + 1.1);
+        // Act
+        int actual = Validering.checkInt(scan);
+        int expected = 1;
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Disabled
+    void checkDate() {
+    }
+}
